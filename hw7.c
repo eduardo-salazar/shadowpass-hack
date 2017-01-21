f1d2a9d1a17a0e773cae488d04c5a5a11e810822
eduardo-salazar <esalazar922@gmail.com>
2017-01-22 07:10:16 +0800

    Finished

--- hw7.c
+++ hw7.c
@@ -13,10 +13,11 @@ int main(void) {
 		fprintf(stderr,"fork error");
 	} else if (pid == 0) {
 		printf("I am a child with pid %d and pgid %d\n",(int)getpid(),(int)getpgid(getpid()));
+		pid_t new = setsid();
+		if (new < 0)
+			perror("setsid error");
+		printf("I am a child after new session with pid %d and pgid %d\n",(int)getpid(),(int)getpgid(getpid()));
 		exit(0);
-		// pid_t new = setsid();
-		// if (new < 0)
-		// 	perror("setsid error");
 	}
 	wait(NULL);
 	printf("Parent Finishing with pid %d and pgid %d\n",(int)getpid(),(int)getpgid(getpid()));
